c3ce1e736764db81a8b9e0f8adb6aa2974cba6e6
Abu Anwar <abuanwar072@gmail.com>
2020-07-11 14:16:34 +0600

    Initial Commit

---
+++ JobFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JobFinder
+//
+//  Created by Abu Anwar MD Abdullah on 11/7/20.
+//  Copyright © 2020 Abu Anwar MD Abdullah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
